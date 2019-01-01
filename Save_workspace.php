8252caece5765dc9fe9c905eec15067926fc1fcc
milad <behrooz1x@gmail.com>
2019-01-02 01:56:29 +0330

    01- Create Basic function method.

---
+++ Save_workspace.php
@@ -0,0 +1,19 @@
+<?php
+
+Class Save_workspace_php {
+    
+    private $file_name = "save_workspace";
+
+    public function set_file_name(){
+
+    }
+
+    public function save_workspace(){
+
+    }
+
+    public function restore_workspace(){
+
+    }
+
+}
