d58008327ccbc71cba8c9c53a76fe604f5e966c1
milad <behrooz1x@gmail.com>
2019-01-02 13:15:21 +0330

    02- Add ignore key array and complete method destination set file output.

--- Save_workspace.php
+++ Save_workspace.php
@@ -1,10 +1,24 @@
 <?php
-
+/*
+ * This class save workspace variable
+ */
 Class Save_workspace_php {
-    
-    private $file_name = "save_workspace";
 
-    public function set_file_name(){
+    //save destination
+    private $file_name = "save_workspace.txt";
+
+    //default key that doesnt save
+    private $ignore_array_key_list = array(
+        "_GET", "_POST", "_COOKIE", "_FILES", "_SERVER", "argv", "argc" //windows or linux simulation default key
+    );
+
+    /*
+     * Change location of save files
+     * Return void
+     */
+    public function set_file_name($file_des){
+        //change file default save path
+        $this->file_name = $file_des;
 
     }
 
