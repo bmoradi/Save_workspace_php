d77864bd4d735a9556046f18681e7413a9c8e41f
milad <behrooz1x@gmail.com>
2019-01-03 12:45:11 +0330

    03- Add two new method and one property($info and is_exist(), delete_workspace).

--- Save_workspace.php
+++ Save_workspace.php
@@ -4,29 +4,62 @@
  */
 Class Save_workspace_php {
 
-    //save destination
-    private $file_name = "save_workspace.txt";
+    //name save file
+    private $workspace_name = "workspace.db";
 
-    //default key that doesnt save
+    //this is about location of the save data
+    public $info = "This is for section ...";
+
+    //default key that does not save
     private $ignore_array_key_list = array(
         "_GET", "_POST", "_COOKIE", "_FILES", "_SERVER", "argv", "argc" //windows or linux simulation default key
     );
-
-    /*
-     * Change location of save files
-     * Return void
-     */
-    public function set_file_name($file_des){
-        //change file default save path
-        $this->file_name = $file_des;
-
-    }
 
     public function save_workspace(){
 
     }
 
     public function restore_workspace(){
+
+    }
+
+    /*
+     * Change location of file save
+     * Return void
+     */
+    public function set_file_name($file_des){
+
+        $this->workspace_name = $file_des;
+    }
+
+    /*
+     * Check workspace file is exist
+     * Return TRUE|FALSE
+     */
+    public function exist_workspace(){
+
+        //Delete cache files
+        clearstatcache();
+
+        if( file_exists($this->workspace_name)){
+            return TRUE;
+        }else{
+            return FALSE;
+        }
+    }
+
+    /*
+     * Delete workspace file
+     * Return TRUE|FALSE
+     */
+    public function delete_workspace(){
+
+        if($this->exist_workspace()){
+            unlink($this->workspace_name);
+            return TRUE;
+        }else{
+            return FALSE;
+        }
 
     }
 
